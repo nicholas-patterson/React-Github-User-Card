6e902cf64c12cf7da557fe2954f010f6ceecc0e3
Nicholas Patterson <nicholaspatterson@Nicholass-MBP.home>
2019-08-06 14:22:20 -0400

    fetched data displaying it to the card

--- react-github-usercard/src/App.js
+++ react-github-usercard/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import GithubCard from "./Components/GithubCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: {}
+    };
+  }
+
+  fetchGithubData = () => {
+    axios
+      .get("https://api.github.com/users/nicholas-patterson")
+      .then(res => this.setState({ userData: res.data }));
+  };
+
+  componentDidMount = () => {
+    this.fetchGithubData();
+  };
+
+  render() {
+    console.log(this.state.userData);
+    return (
+      <div>
+        <GithubCard userData={this.state.userData} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ react-github-usercard/src/Components/GithubCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class GithubCard extends React.Component {
+  render() {
+    return <h1>Hello from Github Card Component</h1>;
+  }
+}
+
+export default GithubCard;
